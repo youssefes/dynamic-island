6153cfb5884a0114ab9f57bfff9dab03636653f3
youssefes <46077105+youssefes@users.noreply.github.com>
2024-10-08 15:31:38 +0300

    Initial Commit

---
+++ dynamic-island/dynamic_islandApp.swift
@@ -0,0 +1,17 @@
+//
+//  dynamic_islandApp.swift
+//  dynamic-island
+//
+//  Created by madar on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct dynamic_islandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
